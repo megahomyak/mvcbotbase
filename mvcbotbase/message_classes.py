169bd4fbaeeaf6ae0b245da438e0ae324e4cfbd8
megahomyak <g.megahomyak@gmail.com>
2021-05-15 13:27:13 +0500

    Made OutgoingMessage, IncomingMessage and IncomingAttachment were modified

--- mvcbotbase/message_classes.py
+++ mvcbotbase/message_classes.py
@@ -1,25 +1,49 @@
 from abc import abstractmethod, ABC
 from dataclasses import dataclass
-from typing import List, Optional
+from io import BytesIO
+from typing import List, Iterable, Optional
 
 
 @dataclass
 class IncomingAttachment:
-    attachment_link: Optional[str]
-    attachment_contents: Optional[str]
-    # attachment_link or attachment_contents should be available
+    info: Optional[dict]
+
+    @abstractmethod
+    async def download(self, path=None) -> Optional[BytesIO]:
+        """
+        Should return BytesIO if path is not specified
+        """
+        pass
 
 
 @dataclass
 class IncomingMessage(ABC):
-    id: int
-    text: str
-    sender_id: int
-    peer_id: int
-    reply_message: Optional['IncomingMessage']
-    forwarded_messages: List['IncomingMessage']
-    attachments: List[IncomingAttachment]
+    id: Optional[int] = None
+    text: Optional[str] = None
+    sender_id: Optional[int] = None
+    peer_id: Optional[int] = None
+    attachments: Optional[List[IncomingAttachment]] = None
 
     @abstractmethod
-    def as_json(self) -> dict:
+    def as_json(self):
+        pass
+
+    @abstractmethod
+    async def get_reply_message(self):
+        pass
+
+    @abstractmethod
+    async def get_forwarded_messages(self):
+        pass
+
+
+@dataclass
+class OutgoingMessage(ABC):
+    text: Optional[str] = None
+    answer_to_message_id: Optional[int] = None
+    peer_id: Optional[int] = None
+    forwarded_messages_ids: Optional[Iterable[int]] = None
+
+    @abstractmethod
+    async def add_attachment(self, attachment: BytesIO):
         pass
