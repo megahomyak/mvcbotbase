acb122088c2b7122140a18bdc017d2cfde125205
megahomyak <g.megahomyak@gmail.com>
2021-05-15 15:07:14 +0500

    Made SocialNetworkProvider

---
+++ mvcbotbase/social_network_provider.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from typing import AsyncGenerator
+
+from message_classes import IncomingMessage, OutgoingMessage
+
+
+class SocialNetworkProvider(ABC):
+
+    @abstractmethod
+    async def get_messages(self) -> AsyncGenerator[IncomingMessage, None]:
+        yield
+
+    @abstractmethod
+    async def send_message(self, message: OutgoingMessage):
+        pass
