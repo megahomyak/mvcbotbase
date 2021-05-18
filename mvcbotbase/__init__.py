173539b6d8577062d315c625a9f3fbd0d14752c3
megahomyak <g.megahomyak@gmail.com>
2021-05-19 00:06:16 +0500

    Made __init__.py

---
+++ mvcbotbase/__init__.py
@@ -0,0 +1,11 @@
+from mvcbotbase.mvcbotbase import MVCBotBase
+from mvcbotbase.social_network_provider import SocialNetworkProvider
+from mvcbotbase.classes_for_command_arguments import (
+    BaseArg, IntArg, WordArg, StringArg, Sequence
+)
+from mvcbotbase.message_classes import (
+    IncomingMessage, OutgoingMessage, AttachmentType, IncomingAttachment
+)
+from mvcbotbase.social_network_provider_implementations.vk_provider import (
+    VKProvider
+)
