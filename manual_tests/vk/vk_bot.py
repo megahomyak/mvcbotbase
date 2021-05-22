8333e4bec73ba7e6259cbb57cb04a47214352bd1
megahomyak <g.megahomyak@gmail.com>
2021-05-23 00:03:31 +0500

    Made two commands to test replies and messages forwarding

--- manual_tests/vk/vk_bot.py
+++ manual_tests/vk/vk_bot.py
@@ -1,7 +1,11 @@
 import json
 
 from mvcbotbase import (
-    MVCBotBase, WordArg, AbstractIncomingMessage, VKProvider, IntArg
+    MVCBotBase, WordArg, AbstractIncomingMessage, IntArg,
+    OutgoingMessage
+)
+from mvcbotbase.social_network_provider_implementations.vk_provider import (
+    VKProvider
 )
 
 config = json.load(open("config.json"))
@@ -16,6 +20,18 @@ mvc_bot_base = MVCBotBase(VKProvider(config["token"], config["group_id"]))
 async def test_handler(
         message: AbstractIncomingMessage, first_arg: str, second_arg: int):
     return "Hello"
+
+
+@mvc_bot_base.add_command("test2")
+async def test_forwarded_messages(message):
+    return OutgoingMessage(message.peer_id, forwarded_messages_ids=[message.id])
+
+
+@mvc_bot_base.add_command("test3")
+async def test_reply(message):
+    return OutgoingMessage(
+        message.peer_id, "abc", reply_to_message_id=message.id
+    )
 
 
 @mvc_bot_base.add_command("help")
