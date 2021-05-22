f2739d6b9525d329127b91f9ae470b308cdec558
megahomyak <g.megahomyak@gmail.com>
2021-05-22 17:33:20 +0500

    Removed VKProvider from __init__.py

--- mvcbotbase/__init__.py
+++ mvcbotbase/__init__.py
@@ -8,6 +8,3 @@ from mvcbotbase.message_classes import (
 )
 from mvcbotbase.mvcbotbase import MVCBotBase
 from mvcbotbase.social_network_provider import SocialNetworkProvider
-from mvcbotbase.social_network_provider_implementations.vk_provider import (
-    VKProvider
-)
