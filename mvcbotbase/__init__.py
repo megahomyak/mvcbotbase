6e93900b20ea6f96a051cf313a2782302aae4dc9
megahomyak <g.megahomyak@gmail.com>
2021-06-22 02:54:09 +0500

    Made __all__

--- mvcbotbase/__init__.py
+++ mvcbotbase/__init__.py
@@ -10,3 +10,14 @@ from mvcbotbase.message_classes import (
 )
 from mvcbotbase.mvcbotbase import MVCBotBase
 from mvcbotbase.social_network_provider import SocialNetworkProvider
+
+__all__ = [
+    "MVCBotBase", "social_network_provider_implementations",
+    "SocialNetworkProvider", "social_network_provider", "BaseArg", "IntArg",
+    "mvcbotbase", "WordArg", "StringArg", "Sequence", "AttachmentType",
+    "OutgoingMessage", "OutgoingAttachment", "OutgoingFileAttachment",
+    "Attachment", "UndownloadableAttachment", "AbstractIncomingAttachment",
+    "AbstractIncomingMessage", "message_classes",
+    "AttachmentToUploadHasUnknownFileTypeError", "ContentCantBeDownloadedError",
+    "classes_for_command_arguments", "command_info"
+]
