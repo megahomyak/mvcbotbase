bf36b8cc066875786ca09a7ff73e959722c8f350
megahomyak <g.megahomyak@gmail.com>
2021-06-22 03:03:19 +0500

    One more fix

--- mvcbotbase/__init__.py
+++ mvcbotbase/__init__.py
@@ -12,12 +12,13 @@ from mvcbotbase.mvcbotbase import MVCBotBase
 from mvcbotbase.social_network_provider import SocialNetworkProvider
 
 __all__ = [
-    "MVCBotBase", "social_network_provider_implementations",
+    "MVCBotBase",
     "SocialNetworkProvider", "social_network_provider", "BaseArg", "IntArg",
     "mvcbotbase", "WordArg", "StringArg", "Sequence", "AttachmentType",
     "OutgoingMessage", "OutgoingAttachment", "OutgoingFileAttachment",
     "Attachment", "UndownloadableAttachment", "AbstractIncomingAttachment",
     "AbstractIncomingMessage", "message_classes",
     "AttachmentToUploadHasUnknownFileTypeError", "ContentCantBeDownloadedError",
-    "classes_for_command_arguments", "command_info"
+    "classes_for_command_arguments", "command_info",
+    "social_network_provider_implementations"
 ]
